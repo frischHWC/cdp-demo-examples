7ece3019fc4e865462fbee4c6958da812cedf577
frisch <frisch@cloudera.com>
2021-12-15 19:14:09 +0100

    feat(pyspark): use on app file

--- pyspark/App.py
+++ pyspark/App.py
@@ -1,16 +1,15 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
-from AppConfig import *
 
 
 def main():
     
-    SparkConf().setAppName(app_name).setMaster(master)
+    SparkConf().setMaster("yarn")
     
     spark = SparkSession \
         .builder \
-        .appName(app_name) \
+        .appName("first_app") \
         .getOrCreate()
 
     rdd = spark.sparkContext.parallelize(range(1, 10000), 10)
@@ -19,7 +18,6 @@ def main():
     df.count()
 
     spark.stop()
-    
 
     
 if __name__ == "__main__":
--- pyspark/AppConfig.py
+++
@@ -1,4 +0,0 @@
-
-# TODO : Add other configurations properties here
-app_name = "spark_py"
-master = "yarn"
